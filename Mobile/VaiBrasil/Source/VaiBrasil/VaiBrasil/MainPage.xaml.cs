4c34f0d208fa549a4511368b103c2079c77839f9
Enielson Saggioratto <e.saggioratto@gmail.com>
2018-06-18 21:16:17 -0300

    ok

---
+++ Mobile/VaiBrasil/Source/VaiBrasil/VaiBrasil/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Xamarin.Forms;
+using VaiBrasil.service;
+using VaiBrasil.service.model;
+
+namespace VaiBrasil
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnConsultar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                ViaCepModel endResult = ViaCepService.GetAdress(CepValido(txtCep));
+                if (endResult != null)
+                {
+                    txtResultado.Text = string.Format("{0} {1},{2}-{3},{4}-{5}", endResult.Logradouro, endResult.Complemento, endResult.Bairro, endResult.Localidade, endResult.Cep, endResult.Uf);
+                }
+                else
+                {
+                    throw new Exception(string.Format("Cep {0} não localizado.", txtCep.Text));
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Erro", ex.Message, "ok");
+            }
+        }
+
+        private string CepValido(Entry _txtCep)
+        {
+
+            if (string.IsNullOrEmpty(_txtCep.Text)) { throw new Exception("Informe um Cep."); }
+
+            Int64 cepNovo = 0;
+            if (!Int64.TryParse(_txtCep.Text.Trim(), out cepNovo)) { throw new Exception("Cep inválido."); }
+
+            if (_txtCep.Text.Length != 8) { throw new Exception("Cep inválido. Deve conter 8 números. Ex: 00000000."); }
+
+            return _txtCep.Text;
+        }
+    }
+}
---
+++ Mobile/VaiBrasil/Source/VaiBrasil/VaiBrasil/service/ViaCepService.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using VaiBrasil.service.model;
+using Newtonsoft.Json;
+namespace VaiBrasil.service
+{
+    public class ViaCepService
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        /// <summary>
+        /// consulta o endereco completo do cep informado como parametro
+        /// </summary>
+        /// <param name="cep"></param>
+        public static ViaCepModel GetAdress(string cep)
+        {
+
+            string novaUrl = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string conteudo = wc.DownloadString(novaUrl);
+
+            ViaCepModel end = JsonConvert.DeserializeObject<ViaCepModel>(conteudo);
+
+            if (string.IsNullOrEmpty(end.Cep))
+            { return null; }
+            else
+            { return end; }
+
+        }
+
+    }
+}
